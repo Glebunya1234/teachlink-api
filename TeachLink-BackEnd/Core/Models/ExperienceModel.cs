0ba2d95915a9c7523ad63c0f5b3c347c7ed2d65e
Glebunya1234 <glebbuckiy@gmail.com>
2025-03-17 17:51:27 +0200

    feat: models

---
+++ TeachLink-BackEnd/Core/Models/ExperienceModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using TeachLink_BackEnd.Core.Entities;
+
+namespace TeachLink_BackEnd.Core.Models
+{
+    [Table("experiences")]
+    public class ExperienceModel : BaseEntityModel
+    {
+        [Column("experience_name")]
+        public string experience_name { get; set; } = string.Empty;
+    }
+}
---
+++ TeachLink-BackEnd/Core/Models/SchoolSubjectsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TeachLink_BackEnd.Core.Models
+{
+    public class SchoolSubjectsModel
+    {
+        public string Subject { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
