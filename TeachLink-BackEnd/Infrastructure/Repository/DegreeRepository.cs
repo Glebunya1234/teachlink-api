a96c0ba4b9866f7592efe33c6e7a50507fb2521d
Glebunya1234 <glebbuckiy@gmail.com>
2025-03-17 17:55:06 +0200

    feat: realization repositories

---
+++ TeachLink-BackEnd/Infrastructure/Repository/DegreeRepository.cs
@@ -0,0 +1,27 @@
+﻿using Supabase;
+using TeachLink_BackEnd.Core.Models;
+using TeachLink_BackEnd.Core.Repositories;
+using TeachLink_BackEnd.Infrastructure.Services;
+
+namespace TeachLink_BackEnd.Core.Services.TeacherService
+{
+    public class DegreeRepository : IDegreeRepository
+    {
+        Client _supabase;
+
+        public DegreeRepository(SupabaseClientFactory supabaseClientFactory)
+        {
+            _supabase = supabaseClientFactory.CreateClient();
+        }
+
+        public async Task<IEnumerable<DegreeModel>?> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DegreeModel?> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TeachLink-BackEnd/Infrastructure/Repository/ReviewRepository.cs
@@ -0,0 +1,37 @@
+﻿using Supabase;
+using TeachLink_BackEnd.Core.Models;
+using TeachLink_BackEnd.Core.Repositories;
+using TeachLink_BackEnd.Infrastructure.Services;
+
+namespace TeachLink_BackEnd.Core.Services.TeacherService
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        Client _supabase;
+
+        public ReviewRepository(SupabaseClientFactory supabaseClientFactory)
+        {
+            _supabase = supabaseClientFactory.CreateClient();
+        }
+
+        public async Task Create(int id_teacher, int id_student, ReviewsModel reviewsModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ReviewsModel>?> GetAll(int id_teacher, int offset, int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ReviewsModel?> GetById(int id_teacher, int id_student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Update(int id_teacher, int id_student, ReviewsModel reviewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
