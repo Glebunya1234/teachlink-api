2d7c4df542afe271000fc0bd1bf06d9488f35660
Glebunya1234 <glebbuckiy@gmail.com>
2025-04-03 15:24:56 +0300

    feat: props notification model

--- TeachLink-BackEnd/Core/ModelsMDB/NotificationsModelMDB.cs
+++ TeachLink-BackEnd/Core/ModelsMDB/NotificationsModelMDB.cs
@@ -1,17 +1,46 @@
-﻿using MongoDB.Bson;
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace TeachLink_BackEnd.Core.ModelsMDB
 {
     public class NotificationsModelMDB : BaseModelMDB
     {
-        [BsonRequired]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string id_teacher { get; set; }
+        private string _id_teacher = null!;
+
+        private string _id_student = null!;
 
         [BsonRequired]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string id_student { get; set; }
+        [Required]
+        public string id_teacher
+        {
+            get => _id_teacher;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("id_teacher cannot be empty.");
+                }
+                _id_teacher = value;
+            }
+        }
+
+        [BsonRequired]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [Required]
+        public string id_student
+        {
+            get => _id_student;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("id_student cannot be empty.");
+                }
+                _id_student = value;
+            }
+        }
 
         public bool is_read { get; set; } = false;
 
