a84b7f088e7ab13e48638428a4445b2d0b59c17d
Glebunya1234 <glebbuckiy@gmail.com>
2025-04-03 14:15:23 +0300

    feat: props student

--- TeachLink-BackEnd/Core/ModelsMDB/StudentsModelMDB.cs
+++ TeachLink-BackEnd/Core/ModelsMDB/StudentsModelMDB.cs
@@ -1,13 +1,37 @@
-﻿namespace TeachLink_BackEnd.Core.ModelsMDB
+﻿using System.Text.RegularExpressions;
+
+namespace TeachLink_BackEnd.Core.ModelsMDB
 {
     public class StudentsModelMDB : BaseModelMDB
     {
-        public string full_name { get; set; } = string.Empty;
+        private string _fullName = null!;
+        private int _age = 0;
 
-        public string city { get; set; } = string.Empty;
+        public string full_name
+        {
+            get => _fullName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Full name must contain at least 1 character.");
+                if (!Regex.IsMatch(value, @"^[a-zA-Zа-яА-Я\s]+$"))
+                    throw new ArgumentException("Full name must contain only letters and spaces.");
 
-        public int age { get; set; } = 0;
+                _fullName = value;
+            }
+        }
+        public string city { get; set; } = null!;
 
-        public string sex { get; set; } = string.Empty;
+        public int age
+        {
+            get => _age;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Age must be greater than 0.");
+                _age = value;
+            }
+        }
+        public string sex { get; set; } = null!;
     }
 }
