b9d2878cb0db17fe82eb1e022bc1a758b6bde943
Glebunya1234 <glebbuckiy@gmail.com>
2025-04-04 18:10:17 +0300

    feat: exp. service

--- TeachLink-BackEnd/Core/Services/ExperienceService/ExperienceService.cs
+++ TeachLink-BackEnd/Core/Services/ExperienceService/ExperienceService.cs
@@ -1,4 +1,5 @@
-﻿using TeachLink_BackEnd.Core.Models;
+﻿using TeachLink_BackEnd.Core.Mappers;
+using TeachLink_BackEnd.Core.ModelsMDB;
 using TeachLink_BackEnd.Core.Repositories;
 
 namespace TeachLink_BackEnd.Core.Services.StudentService
@@ -6,20 +7,27 @@ namespace TeachLink_BackEnd.Core.Services.StudentService
     public class ExperienceService
     {
         private readonly IExperienceRepository _experienceRepository;
+        private readonly IBaseMapper<ExperienceModelMDB, ExperienceDTO> _mapper;
 
-        public ExperienceService(IExperienceRepository experienceRepository)
+        public ExperienceService(
+            IExperienceRepository experienceRepository,
+            IBaseMapper<ExperienceModelMDB, ExperienceDTO> mapper
+        )
         {
             _experienceRepository = experienceRepository;
+            _mapper = mapper;
         }
 
         public async Task<IEnumerable<ExperienceDTO>> GetAll()
         {
-            throw new NotImplementedException();
+            var degrees = await _experienceRepository.GetAll();
+            return _mapper.ToDtoList(degrees);
         }
 
         public async Task<ExperienceDTO?> GetById(string id)
         {
-            throw new NotImplementedException();
+            var degrees = await _experienceRepository.GetById(id);
+            return _mapper.ToDto(degrees);
         }
     }
 }
