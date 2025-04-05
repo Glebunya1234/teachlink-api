c76bcfd763ded1ca197a224e0a02f04ce74312de
Glebunya1234 <glebbuckiy@gmail.com>
2025-04-05 12:52:11 +0300

    feat: create notif. mapper

---
+++ TeachLink-BackEnd/Core/Mappers/CreateNotificationMappers.cs
@@ -0,0 +1,23 @@
+﻿using TeachLink_BackEnd.Core.Mappers;
+using TeachLink_BackEnd.Core.ModelsMDB;
+
+namespace TeachLink_BackEnd.Infrastructure.Services
+{
+    public class CreateNotificationMappers
+        : BaseMapper<NotificationsModelMDB, CreateNotificationDTO>
+    {
+        public override NotificationsModelMDB ToModel(CreateNotificationDTO dto) =>
+            new NotificationsModelMDB
+            {
+                id_teacher = dto.id_teacher,
+                id_student = dto.id_student,
+                for_teacher = dto.for_teacher,
+                is_read = dto.is_read,
+            };
+
+        public override CreateNotificationDTO ToDto(NotificationsModelMDB model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
