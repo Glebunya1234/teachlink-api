90ae77fa1328cf3cfa5ea113da4930d2fe8b42bc
Glebunya1234 <glebbuckiy@gmail.com>
2025-04-05 12:54:01 +0300

    feat: update notif. mapper

---
+++ TeachLink-BackEnd/Core/Mappers/UpdateNotificationMappers.cs
@@ -0,0 +1,16 @@
+﻿using TeachLink_BackEnd.Core.Mappers;
+using TeachLink_BackEnd.Core.ModelsMDB;
+
+namespace TeachLink_BackEnd.Infrastructure.Services
+{
+    public class UpdatNotificationMappers : BaseMapper<NotificationsModelMDB, UpdateNotificationDTO>
+    {
+        public override NotificationsModelMDB ToModel(UpdateNotificationDTO dto) =>
+            new NotificationsModelMDB { is_read = dto.is_read };
+
+        public override UpdateNotificationDTO ToDto(NotificationsModelMDB model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
