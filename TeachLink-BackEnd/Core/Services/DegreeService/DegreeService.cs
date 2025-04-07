d4442abf33c097dd5d2fa1c7b8cc861a3e26a51e
Glebunya1234 <glebbuckiy@gmail.com>
2025-04-07 19:10:31 +0300

    refactor: checks in the degree service

--- TeachLink-BackEnd/Core/Services/DegreeService/DegreeService.cs
+++ TeachLink-BackEnd/Core/Services/DegreeService/DegreeService.cs
@@ -1,33 +1,31 @@
 ﻿using TeachLink_BackEnd.Core.Mappers.BaseMappers;
 using TeachLink_BackEnd.Core.ModelsMDB;
 using TeachLink_BackEnd.Core.Repositories;
-using TeachLink_BackEnd.Infrastructure.Services;
+using TeachLink_BackEnd.Infrastructure.GlobalHendelrs;
 
 namespace TeachLink_BackEnd.Core.Services.StudentService
 {
-    public class DegreeService
+    public class DegreeService(
+        IDegreeRepository degreeRepository,
+        IBaseMapper<DegreeModelMDB, DegreeDTO> mapper
+    )
     {
-        private readonly IDegreeRepository _degreeRepository;
-        private readonly IBaseMapper<DegreeModelMDB, DegreeDTO> _degreeMappers;
-
-        public DegreeService(
-            IDegreeRepository degreeRepository,
-            IBaseMapper<DegreeModelMDB, DegreeDTO> mapper
-        )
-        {
-            _degreeRepository = degreeRepository;
-            _degreeMappers = mapper;
-        }
+        private readonly IDegreeRepository _degreeRepository = degreeRepository;
+        private readonly IBaseMapper<DegreeModelMDB, DegreeDTO> _degreeMappers = mapper;
 
         public async Task<IEnumerable<DegreeDTO>> GetAll()
         {
             var degrees = await _degreeRepository.GetAll();
+            if (degrees.Count() != 0)
+                throw new NotFoundException($"\"Degree\" were not found");
             return _degreeMappers.ToDtoList(degrees);
         }
 
         public async Task<DegreeDTO?> GetById(string id)
         {
-            var degrees = await _degreeRepository.GetById(id);
+            var degrees =
+                await _degreeRepository.GetById(id)
+                ?? throw new NotFoundException($"\"Degree\" with id {id} was not found");
             return _degreeMappers.ToDto(degrees);
         }
     }
