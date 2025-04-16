b8c8d3e6188588f2b1370579f9f45cccb36590d3
Glebunya1234 <glebbuckiy@gmail.com>
2025-04-16 14:44:07 +0300

    test: implement unit tests for announcement model

---
+++ TeachLink-UnitTests/AnnouncementsModelMDBTests.cs
@@ -0,0 +1,76 @@
+﻿using TeachLink_BackEnd.Core.ModelsMDB;
+
+namespace TeachLink_UnitTests
+{
+    [TestClass]
+    public class AnnouncementsModelMDBTests
+    {
+        [TestMethod]
+        [DataRow("605c72ef531123456789abcd", true)]
+        [DataRow("", false)]
+        [DataRow(null, false)]
+        public void SetIdStudent_Validation(string idStudent, bool isValid)
+        {
+            var model = new AnnouncementsModelMDB();
+
+            if (isValid)
+            {
+                model.id_student = idStudent;
+                Assert.AreEqual(idStudent, model.id_student);
+            }
+            else
+            {
+                Assert.ThrowsException<ArgumentException>(() => model.id_student = idStudent);
+            }
+        }
+
+        [TestMethod]
+        [DataRow("This is a valid mini description with more than 20 chars", true)]
+        [DataRow("Short desc", false)]
+        [DataRow("", false)]
+        public void SetMiniDescription_Validation(string desc, bool isValid)
+        {
+            var model = new AnnouncementsModelMDB();
+
+            if (isValid)
+            {
+                model.mini_description = desc;
+                Assert.AreEqual(desc, model.mini_description);
+            }
+            else
+            {
+                Assert.ThrowsException<ArgumentException>(() => model.mini_description = desc);
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(GetDescriptionTestData), DynamicDataSourceType.Method)]
+        public void SetDescription_Validation(string desc, bool isValid)
+        {
+            var model = new AnnouncementsModelMDB();
+
+            if (isValid)
+            {
+                model.description = desc;
+                Assert.AreEqual(desc, model.description);
+            }
+            else
+            {
+                Assert.ThrowsException<ArgumentException>(() => model.description = desc);
+            }
+        }
+
+        public static IEnumerable<object[]> GetDescriptionTestData()
+        {
+            yield return new object[] { new string('a', 100), true };
+            yield return new object[] { new string('a', 99), false };
+            yield return new object[] { new string('a', 500), true };
+            yield return new object[] { new string('a', 501), false };
+            yield return new object[] { "", false };
+        }
+
+        
+    }
+}
+
+
